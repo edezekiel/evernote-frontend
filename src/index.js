5ea09051a6de361f8ff100eeba3419be71e4aa63
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2018-12-21 13:32:59 -0500

    create db.json file and render note titles to page with button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const endPoint = 'http://localhost:3000/api/v1/notes';
+const endPoint = 'http://localhost:3000/api/v1/notes';
+
+document.addEventListener('DOMContentLoaded', initPage);
+
+function initPage(){
+  getNotes()
+}
+
+function getNotes(){
   fetch(endPoint)
     .then(res => res.json())
-    .then(json => console.log(json))
-});
+    .then(notes => notes.forEach(note => renderNote(note)))
+}
+
+function renderNote(note){
+  const markup = `
+  <li>
+    <h3>${note.title}
+    <button>edit</button>
+    </h3>
+  </li>`;
+  document.querySelector('#notes-list').innerHTML += markup;
+}
