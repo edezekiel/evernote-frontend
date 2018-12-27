b1e52fb63d8e65856f30882ce83fb11717a76265
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2018-12-27 14:23:58 -0700

    complete step 5 refactoring

--- src/app.js
+++ src/app.js
@@ -1,23 +1,47 @@
 class App {
-  constructor(){
+  constructor() {
     this.adapter = new Adapter();
+
+    this.handleEditClick = this.handleEditClick.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.createNotes = this.createNotes.bind(this);
+    this.addNotes = this.addNotes.bind(this);
   }
+
 
   attachEventListeners() {
-    document.querySelector('#notes-list').addEventListener('click', e => {
-      const id = e.target.dataset.id;
-      const note = Note.findById(id)
-      document.querySelector("#update").innerHTML = note.renderUpdateForm()
-    });
-
-    document.querySelector('#update').addEventListener('submit', e => {
-       e.preventDefault();
-       const id = e.target.dataset.id;
-       const note = Note.findById(id);
-       const title = e.target.querySelector('input').value;
-       const content = e.target.querySelector('textarea').value;
-       const jsonBody = { title, content };
-       this.adapteber.updateNote(note.id, jsonBody).then(updatedNote => console.log(updatedNote))
-    });
+    document.querySelector('#notes-list').addEventListener('click', this.handleEditClick);
+    document.querySelector('#update').addEventListener('submit', this.handleFormSubmit);
   }
-}
+
+  createNotes(notes) {
+    notes.forEach(note => {
+      new Note(note);
+    });
+    this.addNotes();
+  }
+
+  addNotes() {
+    document.querySelector('#notes-list').innerHTML = '';
+    Note.all.forEach(
+      note => (document.querySelector('#notes-list').innerHTML += note.renderListItem())
+    );
+  }
+
+  handleFormSubmit(e) {
+    e.preventDefault();
+    const id = e.target.dataset.id;
+    const note = Note.findById(id);
+    const title = e.target.querySelector('input').value;
+    const content = e.target.querySelector('textarea').value;
+
+    const bodyJSON = { title, content };
+    this.adapter.updateNote(note.id, bodyJSON).then(updatedNote => console.log(updatedNote));
+  }
+
+    handleEditClick(e) {
+      const id = parseInt(e.target.dataset.id);
+      const note = Note.findById(id);
+      document.querySelector('#update').innerHTML = note.renderUpdateForm();
+    }
+  }
