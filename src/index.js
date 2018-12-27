abb14eb917af2c9d1f42e6540b82ad9313595cd3
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2018-12-27 13:09:47 -0700

    refactor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const endPoint = 'http://localhost:3000/api/v1/notes';
+const endPoint = 'http://localhost:3000/notes';
 
 document.addEventListener('DOMContentLoaded', initPage);
 
@@ -9,15 +9,37 @@ function initPage(){
 function getNotes(){
   fetch(endPoint)
     .then(res => res.json())
-    .then(notes => notes.forEach(note => renderNote(note)))
+    .then(notes =>
+      notes.forEach(note =>
+        renderNote(note)
+      )
+    )
 }
 
 function renderNote(note){
-  const markup = `
-  <li>
-    <h3>${note.title}
-    <button>edit</button>
-    </h3>
-  </li>`;
-  document.querySelector('#notes-list').innerHTML += markup;
+  const ul = document.querySelector("#notes-list")
+  const li = document.createElement('li')
+  const h3 = document.createElement('h3')
+  const button = document.createElement('button')
+
+  button.innerText = "edit"
+  h3.innerText = `${note.title}`
+
+  button.addEventListener('click', editText)
+
+  h3.appendChild(button)
+  li.appendChild(h3)
+  ul.appendChild(li)
 }
+
+function editText(){
+  console.log(event)
+}
+
+// const markup = `
+// <li>
+//   <h3>${note.title}
+//   <button>edit</button>
+//   </h3>
+// </li>`;
+// document.querySelector('#notes-list').innerHTML += markup;
