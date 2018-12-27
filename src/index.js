8fd5385985e6b7b8fd5bcfb274f283d0e2594206
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2018-12-27 13:12:35 -0700

    change the getNotes function

--- src/index.js
+++ src/index.js
@@ -10,36 +10,37 @@ function getNotes(){
   fetch(endPoint)
     .then(res => res.json())
     .then(notes =>
-      notes.forEach(note =>
-        renderNote(note)
-      )
+      notes.forEach(note => {
+        const markup = `
+        <li>
+          <h3>${note.title}
+          <button>edit</button>
+          </h3>
+        </li>`;
+
+        document.querySelector('#notes-list').innerHTML += markup;
+      })
     )
 }
 
-function renderNote(note){
-  const ul = document.querySelector("#notes-list")
-  const li = document.createElement('li')
-  const h3 = document.createElement('h3')
-  const button = document.createElement('button')
 
-  button.innerText = "edit"
-  h3.innerText = `${note.title}`
 
-  button.addEventListener('click', editText)
-
-  h3.appendChild(button)
-  li.appendChild(h3)
-  ul.appendChild(li)
-}
-
-function editText(){
-  console.log(event)
-}
-
-// const markup = `
-// <li>
-//   <h3>${note.title}
-//   <button>edit</button>
-//   </h3>
-// </li>`;
-// document.querySelector('#notes-list').innerHTML += markup;
+// function renderNote(note){
+//   const ul = document.querySelector("#notes-list")
+//   const li = document.createElement('li')
+//   const h3 = document.createElement('h3')
+//   const button = document.createElement('button')
+//
+//   button.innerText = "edit"
+//   h3.innerText = `${note.title}`
+//
+//   button.addEventListener('click', editText)
+//
+//   h3.appendChild(button)
+//   li.appendChild(h3)
+//   ul.appendChild(li)
+// }
+//
+// function editText(){
+//   console.log(event)
+// }
